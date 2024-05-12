93b90ab6da68497482a6da761dae9ef5cc160b56
izzeddin62 <i.ishimwe@alustudent.com>
2024-05-12 17:21:40 +0200

    add 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -19,3 +19,21 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string)=> {
   return `${firstName[0]}. ${lastName}`
 }
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
