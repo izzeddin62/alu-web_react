d1033aee3adceaf8d6cd9205c36e874c3b763bf4
izzeddin62 <i.ishimwe@alustudent.com>
2024-05-12 17:12:16 +0200

    modify 1

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,7 +1,8 @@
-interface Teacher extends Record<string, any> {
-  firstName: string;
-  lastName: string;
+interface Teacher{
+  readonly firstName: string;
+  readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number
   location: string;
+  [propName: string]: any;
 }
