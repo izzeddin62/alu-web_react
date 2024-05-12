9a4ebf20f6160f69c1761c1e60e9408fa46ae23e
izzeddin62 <i.ishimwe@alustudent.com>
2024-05-12 17:19:43 +0200

    add 3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -10,3 +10,12 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string)=> {
+  return `${firstName[0]}. ${lastName}`
+}
