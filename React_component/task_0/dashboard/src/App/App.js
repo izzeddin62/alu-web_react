4846f770eeba2b3747377287378d1ed9cb011a3b
izzeddin62 <i.ishimwe@alustudent.com>
2024-05-25 08:53:39 +0200

    add 0

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -8,23 +8,24 @@ import CourseList from "../CourseList/CourseList";
 import { getLatestNotifications } from "../utils/utils";
 
 export default class App extends React.Component {
-  render() {
-    const isLoggedIn = false;
-    const listCourses = [
+  isLoggedIn = false;
+    listCourses = [
       { id: 1, name: "ES6", credit: 60 },
       { id: 2, name: "Webpack", credit: 20 },
       { id: 3, name: "React", credit: 40 },
     ];
-    const listNotifications = [
+    listNotifications = [
       { id: 1, type: "default", value: "New course available" },
       { id: 2, type: "urgent", value: "New resume available" },
       { id: 3, type: "urgent", html: getLatestNotifications() },
     ];
+  render() {
+  
     return (
       <div className="app">
-        <Notifications listNotifications={listNotifications} />
+        <Notifications listNotifications={this.listNotifications} />
         <Header />
-        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        {this.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
         <Footer />
       </div>
     );
