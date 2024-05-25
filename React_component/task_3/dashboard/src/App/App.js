ab2bb92f7ceac86bde40eb8a6920ec4905322c70
izzeddin62 <i.ishimwe@alustudent.com>
2024-05-25 09:40:14 +0200

    add 5

--- React_component/task_3/dashboard/src/App/App.js
+++ React_component/task_3/dashboard/src/App/App.js
@@ -7,36 +7,43 @@ import Login from "../Login/Login";
 import CourseList from "../CourseList/CourseList";
 import { getLatestNotifications } from "../utils/utils";
 import PropTypes from "prop-types";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import BodySection from "../BodySection/BodySection";
 
 export default class App extends React.Component {
   isLoggedIn = false;
-    listCourses = [
-      { id: 1, name: "ES6", credit: 60 },
-      { id: 2, name: "Webpack", credit: 20 },
-      { id: 3, name: "React", credit: 40 },
-    ];
-    listNotifications = [
-      { id: 1, type: "default", value: "New course available" },
-      { id: 2, type: "urgent", value: "New resume available" },
-      { id: 3, type: "urgent", html: getLatestNotifications() },
-    ];
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotifications() },
+  ];
 
 
-    componentDidMount() {
-      window.addEventListener("keydown", (event) => {
-        if (event.ctrlKey && event.key === "h") {
-          alert("Logging you out");
-          this.props.logOut();
-        }
-      });
-    }
+  componentDidMount() {
+    window.addEventListener("keydown", (event) => {
+      if (event.ctrlKey && event.key === "h") {
+        alert("Logging you out");
+        this.props.logOut();
+      }
+    });
+  }
   render() {
-  
+
     return (
       <div className="app">
         <Notifications listNotifications={this.listNotifications} />
         <Header />
-        {this.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
+        <BodySectionWithMarginBottom title={this.isLoggedIn ? 'Course list' : "Log in to continue"}>
+          {this.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
+        </BodySectionWithMarginBottom>
+        <BodySection title="News from the School">
+          <p>Latest school gossip</p>
+        </BodySection>
         <Footer />
       </div>
     );
@@ -44,7 +51,7 @@ export default class App extends React.Component {
 }
 
 App.defaultProps = {
-  logOut: () => {},
+  logOut: () => { },
 };
 
 App.propTypes = {
