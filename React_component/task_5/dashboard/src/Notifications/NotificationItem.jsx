b5a4eafdec760506066bb8e35edbb21f91ab7a72
izzeddin62 <i.ishimwe@alustudent.com>
2024-05-25 09:53:53 +0200

    add 9

--- React_component/task_5/dashboard/src/Notifications/NotificationItem.jsx
+++ React_component/task_5/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,4 +1,6 @@
-export default function NotificationItem({ type, value, html, markAsRead, id }) {
+import React, { memo } from 'react';
+
+function NotificationItem({ type, value, html, markAsRead, id }) {
   if (html) {
     return (
       <li onClick={() => markAsRead(id)} data-priority={type} dangerouslySetInnerHTML={{ __html: html }}></li>
@@ -7,3 +9,5 @@ export default function NotificationItem({ type, value, html, markAsRead, id }) 
     return <li onClick={() => markAsRead(id)} data-priority={type}>{value}</li>;
   }
 }
+
+export default memo(NotificationItem);
