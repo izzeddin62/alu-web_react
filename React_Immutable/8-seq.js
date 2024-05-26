14a32a9be26933217004e2c34ad1c87bfd350641
izzeddin62 <i.ishimwe@alustudent.com>
2024-05-26 17:29:06 +0200

    fix 8

--- React_Immutable/8-seq.js
+++ React_Immutable/8-seq.js
@@ -2,8 +2,8 @@ const { Seq } = require('immutable');
 
 export default function printBestStudents(grades) {
   const bestStudents = Seq(grades)
-    .filter(student => student.score >= 70)
-    .map(student => ({
+    .filter((student) => student.score >= 70)
+    .map((student) => ({
       ...student,
       firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
       lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
