14c4311b4454070d85527c6c0537fd4575c56f5d
izzeddin62 <i.ishimwe@alustudent.com>
2024-05-26 16:42:53 +0200

    fix 2

--- React_Immutable/2-nested.js
+++ React_Immutable/2-nested.js
@@ -1,13 +1,15 @@
+import { getIn } from 'immutable';
 export default function accessImmutableObject(object, array) {
-  let result = null;
-  array.map((key) => {
-    if (result === null) {
-      result = object[key];
-    } else if (result !== null && typeof result === 'object') {
-      result = result[key];
-    } else {
-      result = undefined;
-    }
-});
-  return result;
+  return getIn(object, array);
 }
+
+
+const res = accessImmutableObject({
+    name: {
+         first: "Guillaume",
+         last: "Salva"
+    }
+}, ['name', 'first'])
+
+
+console.log(res)
