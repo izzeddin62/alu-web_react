e7282bed6dd5215201883db549a5c99d281ca04c
izzeddin62 <i.ishimwe@alustudent.com>
2024-05-26 12:44:49 +0200

    add tests

--- react_state/task_0/dashboard/src/App/App.test.js
+++ react_state/task_0/dashboard/src/App/App.test.js
@@ -1,5 +1,6 @@
 import { StyleSheetTestUtils } from "aphrodite";
 import App from "./App";
+import { shallow } from "enzyme";
 import { render, screen } from '@testing-library/react';
 
 StyleSheetTestUtils.suppressStyleInjection()
@@ -9,5 +10,37 @@ describe('App tests', () => {
         screen.getByTestId('App-header')
         screen.getByTestId('App-body')
         screen.getByTestId('App-footer')
-    })
+    });
+
+
+    it.skip("Has default state for displayDrawer false", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.state().displayDrawer).toEqual(false);
+    });
+
+    it.skip("displayDrawer changes to true when calling handleDisplayDrawer", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.state().displayDrawer).toEqual(false);
+
+        const instance = wrapper.instance();
+
+        instance.handleDisplayDrawer();
+
+        expect(wrapper.state().displayDrawer).toEqual(true);
+    });
+
+    it.skip("displayDrawer changes to false when calling handleHideDrawer", () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.state().displayDrawer).toEqual(false);
+
+        // const instance = wrapper.instance();
+
+        wrapper.instance().handleDisplayDrawer();
+
+        expect(wrapper.state().displayDrawer).toEqual(true);
+
+        wrapper.instance().handleHideDrawer();
+
+        expect(wrapper.state().displayDrawer).toEqual(false);
+    });
 })
