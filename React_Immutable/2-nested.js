b303a0efc4861ddef85f8a4d1b6b716a924ee81d
izzeddin62 <i.ishimwe@alustudent.com>
2024-05-26 16:55:00 +0200

    fix 2

--- React_Immutable/2-nested.js
+++ React_Immutable/2-nested.js
@@ -1,4 +1,5 @@
-import { getIn } from 'immutable';
+import { fromJS } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-  return getIn(object, array);
+  return fromJS(object).getIn(array);
 }
