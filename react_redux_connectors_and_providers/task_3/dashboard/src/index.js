ef0ac040b27bc382f74d46395ec52e0b65b0ee7c
izzeddin62 <i.ishimwe@alustudent.com>
2024-06-12 15:23:02 +0200

    add 1

---
+++ react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App/App';
+import { createStore, applyMiddleware } from 'redux';
+import { uiReducer } from './reducers/uiReducer';
+import { Provider } from 'react-redux';
+import { thunk } from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+
+const store = createStore(uiReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+
